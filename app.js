e344d781fc2a62ad2abc3d938109b21b9f7e3401
SergioJss <sergiosilvasalas@gmail.com>
2019-11-05 11:04:55 -0500

    added b.parser, conf middleware, fin route & act

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.get('/', function(req, res){
     res.render('home.ejs');
 });
 
-app.post('/whatever-we-want', function(req, res){
-    console.log('Hitting')
+app.post('/', function(req, res){
+    console.log(req.body.city)
+    res.end();
 });
 
 app.listen(3000, function(){
