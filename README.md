86d5664e7a4ad6c69210a2691e2cef8f7546a186
SergioJss <sergiosilvasalas@gmail.com>
2019-11-05 10:47:12 -0500

    installing body parser for optimized functions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Weather-now
+
+
+get user input of the city
+get weather for the city
+    if error let the user know there was an err 
+    of there is no weather available, err
+    else store the weather for the user city
+show the weather or err
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.set('view engine', 'ejs');
 
 //middleware
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: false}))
 
 /*
     ROUTES
@@ -15,6 +17,10 @@ app.use(express.static('./public'));
 // GEt/
 app.get('/', function(req, res){
     res.render('home.ejs');
+});
+
+app.post('/whatever-we-want', function(req, res){
+    console.log('Hitting')
 });
 
 app.listen(3000, function(){
